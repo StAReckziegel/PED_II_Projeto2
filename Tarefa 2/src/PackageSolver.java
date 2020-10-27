5b806254578e96b023b7f6754e273a232282b9d0
StAR <sareckziegel@universo.univates.br>
2020-10-27 19:52:07 -0300

    Inclusão da estrutur State/Solver obrigatória.

---
+++ Tarefa 2/src/PackageSolver.java
@@ -0,0 +1,49 @@
+public class PackageSolver
+{
+    // contador de estados gerados
+    public static int count = 0;
+    
+    // registra o melhor estado encontrado até agora
+    public static PackageState best = new PackageState("");
+
+    public static void solve(int level, PackageState state)
+    {
+        if (level == 0) {
+            // apenas serão gerados estados com 'level' caracteres
+            if (state.isValid() == false) {
+                // como estado não é válido, retorna imediatamente
+                return; 
+            }
+            System.out.println(state + "=" + state.evaluate());
+            
+            if (best.toString().equals("")) {
+                // 'best' deve ser inicializado com o primeiro estado criado
+                best = state;
+            }
+            if (state.evaluate() > best.evaluate()) {
+                // se o estado atual é melhor, atualiza
+                best = state;
+            }
+            count++; 
+        }
+        else {
+            // baseado no estado parcial atual, gera e testa diferentes variações
+            solve(level - 1, new PackageState(state.toString() + "a"));
+            solve(level - 1, new PackageState(state.toString() + "b"));
+            solve(level - 1, new PackageState(state.toString() + "c")); 
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        // procura a melhor sequência com exatos 6 caracteres
+        long start = System.currentTimeMillis();
+        solve(6, new PackageState(""));
+        
+        System.out.println();
+        System.out.println("count: " + count);
+        System.out.println("time : " + (System.currentTimeMillis() - start) / 1000.0 + " s");        
+        System.out.println("best : " + best + " [" + best.evaluate() + "]");
+    }
+}
+
---
+++ Tarefa 2/src/PackageState.java
@@ -0,0 +1,38 @@
+public class PackageState
+{
+    private String state;
+    
+    public PackageState(String s) {
+        this.state = s;
+    }
+    
+    public boolean isValid() {
+        // restricão: define qual é um estado válido
+        // neste caso, é inválido se conter mais de 2 caracteres 'c'
+        int c = 0;
+        for (int i = 0; i < this.state.length(); i++) {
+            if (this.state.charAt(i) == 'c') { 
+                c++; 
+            }
+        }
+        return c <= 2;
+    }
+   
+    public double evaluate() {
+        // avaliação: define uma função de valor para este estado, que
+        // deve ser maximizada ou minimizada, dependendo do problema
+        // neste caso, simplesmente adiciona pesos para cada caracter
+        double r = 0;
+        for (int i = 0; i < this.state.length(); i++) {
+            if (this.state.charAt(i) == 'a') { r +=  2.1; }
+            if (this.state.charAt(i) == 'b') { r += -1.5; }
+            if (this.state.charAt(i) == 'c') { r +=  4.0; }
+        }
+        return r;
+    }
+    
+    public String toString() {
+        return this.state; 
+    }
+}
+
